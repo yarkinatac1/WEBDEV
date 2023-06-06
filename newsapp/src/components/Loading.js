7099f15eb366a86cbdd7a9a0c6f44948bd376c8a
Yarkın Ataç <yarkin@car4future.link>
2023-06-06 17:42:33 +0300

    NewsApp with using React, NodeJS and newsAPI

---
+++ newsapp/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import loading from "../loading.gif";
+
+const Loading = () => {
+  return (
+    <div className="text-center">
+      <img width={"300"} height={"200"} src={loading} alt="loading" />
+    </div>
+  )
+}
+export default Loading
